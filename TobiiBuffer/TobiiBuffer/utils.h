ed8d54432f55b0067cebf25fa6d6415ccc487998
Diederick C. Niehorster <dcnieho@gmail.com>
2018-09-11 16:51:36 +0200

    added more informative text to each error message, as its already in the Tobii header file

--- TobiiBuffer/TobiiBuffer/utils.h
+++ TobiiBuffer/TobiiBuffer/utils.h
@@ -2,7 +2,8 @@
 #include <string>
 #include <tobii_research.h>
 
-std::string TobiiResearchStatusToString(TobiiResearchStatus trs_);
+std::string TobiiResearchStatusToString     (TobiiResearchStatus trs_);
+std::string TobiiResearchStatusToExplanation(TobiiResearchStatus trs_);
 
 // the below function is called when an error occurred and application execution should halt
 // this function is not defined in this library, it is for the user to implement depending on his platform
--- TobiiBuffer/tools/TobiiResearchStatusLister.m
+++ TobiiBuffer/tools/TobiiResearchStatusLister.m
@@ -1,6 +1,7 @@
 file = '..\TobiiCSDK\64\include\tobii_research.h';
+path = fileparts(mfilename('fullpath'));
 
-fid = fopen(file,'r');
+fid = fopen(fullfile(path,file),'rt');
 txt = fread(fid,'*char').';
 fclose(fid);
 
@@ -10,9 +11,14 @@ idxend = find(txt=='}'); idxend = idxend(find(idxend>idx,1));
 constants = regexp(txt(idx:idxend),'TOBII_RESEARCH_STATUS_(\w+)','tokens');
 constants = cat(1,constants{:});
 
+errorMsgs = regexp(txt(idx:idxend),'/\*\*\n\s*([\w\s]+)','tokens');
+errorMsgs = cat(1,errorMsgs{:});
+
+assert(length(constants)==length(errorMsgs));
+
 % now generate some C code to build the lookup map contents
 str = [];
 for p=1:size(constants,1)
     fullstr = sprintf('TOBII_RESEARCH_STATUS_%s',constants{p});
-    str = [str sprintf('    case %1$s:\n        return TobiiResearchStatusInfo{"%1$s", static_cast<int>(%1$s)};\n',fullstr)];
+    str = [str sprintf('    case %1$s:\n        return TobiiResearchStatusInfo{"%1$s", static_cast<int>(%1$s), "%2$s"};\n',fullstr,errorMsgs{p})];
 end
