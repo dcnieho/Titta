f8200187bed5bc72a812c44e90e9c4b1e0af86df
Diederick Niehorster <dcnieho@gmail.com>
2018-09-24 16:54:56 +0200

    don't forget the break statement in the switch, even if case otherwise not implemented

--- TobiiBuffer/TobiiBuffer_websocket/main.cpp
+++ TobiiBuffer/TobiiBuffer_websocket/main.cpp
@@ -115,6 +115,7 @@ int main() {
                 break;
             case Action::SetSampleFreq:
             {
+                break;
             }
             case Action::StartSampleStream:
             {
