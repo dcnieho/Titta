2f23a18debbf848de570f8d87000c50c3d1c8b16
Diederick Niehorster <dcnieho@gmail.com>
2019-05-09 15:39:59 +0200

    made start with TalkToProLab (External Presenter) interface

---
+++ talkToProLab/SimpleWSClient.m
@@ -0,0 +1,191 @@
+classdef SimpleWSClient < WebSocketClient
+    properties (SetAccess = protected)
+        lastRespText = '';
+        lastRespBinary = [];
+    end
+    
+    methods
+        function this = SimpleWSClient(varargin)
+            % add jar to static class path if needed, so the websocket java
+            % library can be found
+            jarPath     = which('matlab-websocket-1.4.jar');
+            mustRestart = setupJavaStaticClassPath(jarPath);
+            if mustRestart
+                error('restart matlab now')
+            end
+            
+            % Constructor
+            this@WebSocketClient(varargin{:});
+        end
+        
+        function resp = get.lastRespText(this)
+            resp = matlab.internal.webservices.fromJSON(this.lastRespText);
+            this.lastRespText = '';
+        end
+        function resp = get.lastRespBinary(this)
+            resp = this.lastRespBinary;
+            this.lastRespBinary = [];
+        end
+    end
+    
+    methods (Access = protected)
+        function onOpen(this,message)
+            % This function simply displays the message received
+            fprintf('%s\n',message);
+        end
+        
+        function onTextMessage(this,message)
+            % This function simply displays the message received
+            this.lastRespText = message;
+        end
+        
+        function onBinaryMessage(this,bytearray)
+            % This function simply displays the message received
+            this.lastRespBinary = bytearray;
+        end
+        
+        function onError(this,message)
+            % This function simply displays the message received
+            fprintf('Error: %s\n',message);
+        end
+        
+        function onClose(this,message)
+            % This function simply displays the message received
+            fprintf('%s\n',message);
+        end
+    end
+end
+
+
+function mustRestart = setupJavaStaticClassPath(jarPath)
+
+mustRestart = false;
+try
+    qDoInstall = isempty(which('io.github.jebej.matlabwebsocket.MatlabWebSocketClient'));
+catch
+    % can't import, try and fix up path
+    qDoInstall = true;
+end
+
+if ~qDoInstall
+    return;
+end
+
+% this setup code is taken from PsychJavaTrouble.m
+try
+    % Matlab version 8.1 (R2013a) or later. classpath.txt can't be
+    % used anymore. Now they want us to store static classpath
+    % definitions in a file called javaclasspath.txt inside the
+    % Matlab preference folder:
+    
+    % Try to find the file, if it already exists, e.g., inside the
+    % Matlab startup folder:
+    classpathFile = which('javaclasspath.txt');
+    
+    % Found it?
+    if isempty(classpathFile)
+        % Nope. So we try the preference folder.
+        % Retrieve path to preference folder. Create the folder if it
+        % doesn't already exist:
+        prefFolder = prefdir(1);
+        classpathFile = [prefFolder filesep 'javaclasspath.txt'];
+        if ~exist(classpathFile, 'file')
+            fid = fopen(classpathFile, 'w');
+            fclose(fid);
+        end
+    end
+    
+    % Define name of backup file:
+    bakclasspathFile = [classpathFile '.bak'];
+    
+    % read each line into separate cell
+    fid = fopen(classpathFile);
+    fileContentsWrapped = textscan(fid, '%s', 'delimiter', '\n');
+    fclose(fid);
+    fileContents = fileContentsWrapped{1};
+    
+    j = 1;
+    newFileContents = {};
+    pathInserted = 0;
+    for i = 1:length(fileContents)
+        % Look for the first instance of PsychJava in the classpath and
+        % replace it with the new one.  All other instances will be
+        % removed.
+        if isempty(strfind(fileContents{i}, 'matlab-websocket'))
+            newFileContents{j, 1} = fileContents{i}; %#ok<AGROW>
+            j = j + 1;
+        elseif ~isempty(strfind(fileContents{i}, 'matlab-websocket'))
+            if ~pathInserted
+                newFileContents{j, 1} = jarPath; %#ok<AGROW>
+                pathInserted = 1;
+                j = j + 1;
+            else
+                % don't copy over
+            end
+        end
+    end
+    
+    % If the PsychJava path wasn't inserted, then this must be a new
+    % installation, so we append it to the classpath.
+    if ~pathInserted
+        newFileContents{end + 1, 1} = jarPath;
+    end
+    
+    % Now compare to see if the new and old classpath are the same.  If
+    % they are, then there's no need to do anything.
+    updateClasspath = 1;
+    if length(fileContents) == length(newFileContents)
+        if strcmp(fileContents, newFileContents)
+            updateClasspath = 0;
+        end
+    end
+    
+    if updateClasspath
+        % Make a backup of the old classpath.
+        clear madeBackup;
+        
+        [s, w] = copyfile(classpathFile, bakclasspathFile, 'f');
+        
+        if s==0
+            error(['Could not make a backup copy of Matlab''s JAVA path definition file. The system reports: ', w]);
+        end
+        madeBackup = 1; %#ok<NASGU>
+        
+        % Write out the new contents.
+        fid = fopen(classpathFile, 'w');
+        if fid == -1
+            error('Could not open Matlab''s JAVA path definition file for write access.');
+        end
+        for i = 1:length(newFileContents)
+            fprintf(fid, '%s\n', newFileContents{i});
+        end
+        fclose(fid);
+        
+        fprintf('\n\n');
+        disp('*** Matlab''s Static Java classpath definition file modified. You will have to restart Matlab to enable use of the new Java components. ***');
+        fprintf('\nPress RETURN or ENTER to confirm you read and understood the above message.\n');
+        pause;
+    end
+catch %#ok<CTCH>
+    lerr = psychlasterror;
+    fprintf('Could not update the Matlab Java classpath file due to the following error:\n');
+    fprintf('%s\n\n', lerr.message);
+    fprintf('You likely do not have sufficient access permissions for the Matlab application\n');
+    fprintf('folder or file itself to change the file %s .\n', classpathFile);
+    fprintf('Please ask the system administrator to enable write-access to that file and its\n');
+    fprintf('containing folder and then repeat the update procedure.\n');
+    fprintf('Alternatively, ask the administrator to add the following line:\n');
+    fprintf('%s\n', jarPath);
+    fprintf('to the file: %s\n\n', classpathFile);
+    fprintf('TalkToProLab will <b><u>not work</b></u> if you skip this step, Titta will still\n');
+    fprintf('be functional, \n');
+    fprintf('\nPress RETURN or ENTER to confirm you read and understood the above message.\n');
+    pause;
+    
+    % Restore the old classpath file if necessary.
+    if exist('madeBackup', 'var')
+        [s, w] = copyfile(bakclasspathFile, classpathFile, 'f'); %#ok<ASGLU>
+    end
+end
+mustRestart = true;
+end
---
+++ talkToProLab/TalkToProLab.m
@@ -0,0 +1,145 @@
+classdef TalkToProLab < handle
+    properties (Access = protected, Hidden = true)
+        % websocket connections we need
+        clientClock;
+        clientProject;
+        clientEP;
+    end
+    
+    properties (SetAccess=protected)
+        projectID;
+        participantID;
+        recordingID;
+    end
+    
+    % computed properties (so not actual properties)
+    properties (Dependent, SetAccess = private)
+    end
+    
+    methods
+        function this = TalkToProLab()
+            % connect to needed Pro Lab Services
+            this.clientClock    = SimpleWSClient('ws://localhost:8080/clock?client_id=TittaMATLAB');
+            this.clientProject  = SimpleWSClient('ws://localhost:8080/project?client_id=TittaMATLAB');
+            this.clientEP       = SimpleWSClient('ws://localhost:8080/record/externalpresenter?client_id=TittaMATLAB');
+            
+            % check our local clock is the same as the ProLabClock. for now
+            % we do not support it when they aren't (e.g. running lab on a
+            % different machine than the stimulus presentation machine)
+            nTimeStamp = 40;
+            [timesPTB,timesLab] = deal(zeros(nTimeStamp,1,'int64'));
+            request = matlab.internal.webservices.toJSON(struct('operation','GetTimestamp'));
+            % ensure response is cleared
+            [~] = this.clientClock.lastRespText;
+            for p=1:nTimeStamp
+                if mod(p-1,10)<5
+                    PTBtime = GetSecs;
+                    this.clientClock.send(request);
+                else
+                    this.clientClock.send(request);
+                    PTBtime = GetSecs;
+                end
+                % prep PTB timestamp
+                timesPTB(p) = int64(PTBtime*1000*1000);
+                % wait for response
+                resp = waitForResponse(this.clientClock,'GetTimestamp');
+                timesLab(p) = sscanf(resp.timestamp,'%ld');
+            end
+            % get rough estimate of clock offset (note this is includes
+            % half RTT which is not taken into account, thats ok for our
+            % purposes)
+            assert(mean(timesLab-timesPTB)<2500,'clock offset between PsychToolbox and Pro Lab is more than 2.5 ms: either the two are not using the same clock (unsupported) or you are running PsychToolbox and Pro Lab on different computers (also unsupported)')
+            
+            % get info about opened project
+            this.clientProject.send(matlab.internal.webservices.toJSON(struct('operation','GetProjectInfo')));
+            resp = waitForResponse(this.clientProject,'GetProjectInfo');
+            this.projectID = resp.project_id;
+            fprintf('Connected to Tobii Pro Lab, currently opened project is ''%s'' (%s)\n',resp.project_name,resp.project_id);
+        end
+        
+        function participantID = createParticipant(this,name,allowExisting)
+            if nargin<3 || isempty(allowExisting)
+                allowExisting = false;
+            end
+            % get list of existing participants, see if one with name
+            % already exists
+            this.clientProject.send(matlab.internal.webservices.toJSON(struct('operation','ListParticipants')));
+            resp = waitForResponse(this.clientProject,'ListParticipants');
+            names = {resp.participant_list.participant_name};
+            qPart = strcmp(names,name);
+            assert(~any(qPart)||allowExisting,'Participant with name ''%s'' already exists',name)
+            
+            if any(qPart)
+                % use existing
+                participantID = resp.participant_list(qPart).participant_id;
+            else
+                % make new
+                this.clientProject.send(matlab.internal.webservices.toJSON(struct('operation','AddParticipant','participant_name',name)));
+                resp = waitForResponse(this.clientProject,'AddParticipant');
+                participantID = resp.participant_id;
+            end
+            this.participantID = participantID;
+        end
+        
+        function ID = uploadStimulus(this,fileNameOrArray,name)
+        end
+        
+        function findStimulus(this,name)
+        end
+    end
+    
+    methods (Access=private, Hidden=true)
+    end
+end
+
+% for complicated messages, perhaps provide users with an empty template.
+% like AOI:
+% fid=fopen('C:\Users\Administrator\Desktop\json.txt','rt');
+% str=fread(fid,inf,'*char').'
+% fclose(fid);
+% matlab.internal.webservices.fromJSON(str)
+
+
+%%% helpers
+function resp = waitForResponse(connection, operation, pauseDur)
+if nargin<3 || isempty(pauseDur)
+    pauseDur = 0.001;
+end
+% wait for response
+resp = connection.lastRespText;
+while isempty(resp) || ~(resp.status_code~=0 || strcmp(resp.operation,operation))
+    resp = connection.lastRespText;
+    pause(pauseDur);
+end
+
+if resp.status_code
+    reason = '';
+    if isfield(resp,'reason')
+        reason = sprintf(': %s',resp.reason);
+    end
+    error('Command ''%s'' returned an error: %d (%s)%s',operation,resp.status_code,statusToText(resp.status_code),reason);
+end
+end
+
+function str = statusToText(status)
+switch status
+    case 0
+        str = 'Operation successful';
+    case 100
+        str = 'Bad request';
+    case 101
+        str = 'Invalid parameter';
+    case 102
+        str = 'Operation was unsuccessful';
+    case 103
+        str = 'Operation cannot be executed in current state';
+    case 104
+        str = 'Access to the service is forbidden';
+    case 105
+        str = 'Authorization during connection to a service has not been provided';
+    case 201
+        str = 'Recording finalization failed';
+    otherwise
+        str = '!!unknown status code';
+end
+end
