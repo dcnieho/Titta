13d605a2ee0193648ea37986641dc6d863b80e0b
Diederick C. Niehorster <dcnieho@gmail.com>
2019-05-10 00:40:35 +0200

    rgb2hsl didn't work for yellow, got a different implementation

--- utils/rgb2hsl.m
+++ utils/rgb2hsl.m
@@ -3,25 +3,31 @@ function hsl = rgb2hsl(rgb)
 rgb = rgb(:,1:3)/255;
 mx=max(rgb,[],2);%max of the 3 colors
 mn=min(rgb,[],2);%min of the 3 colors
-d = mx - mn;
 
-% luminance
 L = (mx + mn) / 2;
-
-% saturation
-S = d./(mx + mn).*(L <= 0.5) + d./(2-mx-mn).*(L > 0.5);
-
-% Hue
-H = 0 ...
-    + (mx == rgb(:,1)) .* ((rgb(:,2) - rgb(:,3))./d + 6.*(rgb(:,2)<rgb(:,3))) ...
-    + (mx == rgb(:,2)) .* ((rgb(:,3) - rgb(:,1))./d + 2)                      ...
-    + (mx == rgb(:,3)) .* ((rgb(:,1) - rgb(:,2))./d + 4);
-H = H/6;
-
-% correct for achromatic
-gray = (mn == mx);
-S(gray) = 0;
-H(gray) = 0;
+if (mx == mn)
+    H = 0;
+    S = 0;
+else
+    d = mx - mn;
+    if L > 0.5
+        S = d / (2 - mx - mn);
+    else
+        S = d / (mx + mn);
+    end
+    switch (mx)
+        case rgb(1)
+            H = (rgb(2) - rgb(3)) / d;
+            if rgb(2)<rgb(3)
+                H = H+6;
+            end
+        case rgb(2)
+            H = (rgb(3) - rgb(1)) / d + 2;
+        case rgb(3)
+            H = (rgb(1) - rgb(2)) / d + 4;
+    end
+    H = H/6;
+end
 
 % output (all [0 1] range), maps onto 0-360 deg for H, 0-100% for the
 % others
