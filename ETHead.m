23fab0b05405f9e831f7b83bd1f0426c3b5b6014
Diederick C. Niehorster <dcnieho@gmail.com>
2019-07-17 01:53:45 +0200

    upgraded ETHead to main class, documented reference to cite. Added demo

--- ETHead.m
+++ ETHead.m
@@ -1,33 +1,43 @@
-% shut up all warnings about accessing other class properties in a class's
-% setter or getter
+% This class is part of Titta, a toolbox providing convenient access to
+% eye tracking functionality using Tobii eye trackers
+%
+% Titta can be found at https://github.com/dcnieho/Titta. Check there for
+% the latest version.
+% When using Titta, please cite the following paper:
+% Niehorster, D.C., Andersson, R. & Nyström, M., (in prep). Titta: A
+% toolbox for creating Psychtoolbox and Psychopy experiments with Tobii eye
+% trackers.
+%
+% To run demo, simply call ETHead() with no input arguments.
+
 classdef ETHead < handle
     properties
         % setup head position visualization
-        distGain    = 1.5;
-        eyeSzFac    = .25;
-        eyeMarginFac= .25;
-        pupilSzFac  = .50;
-        pupilRefDiam= 5;    % mm
-        pupilSzGain = 1.5;
+        distGain            = 1.5;
+        eyeSzFac            = .25;
+        eyeMarginFac        = .25;
+        pupilSzFac          = .50;
+        pupilRefDiam        = 5;    % mm
+        pupilSzGain         = 1.5;
         
         refSz;
         rectWH;
         
-        headCircleFillClr
-        headCircleEdgeClr
+        headCircleFillClr   = [255 255 0 .3*255];
+        headCircleEdgeClr   = [255 255 0];
         headCircleEdgeWidth = 5;
         
-        crossEye = 0;   % 0: none, 1: replace left eye with cross, 2: replace right eye with cross
+        crossEye            = 0;   % 0: none, 1: replace left eye with cross, 2: replace right eye with cross
         
-        showEyes
-        showPupils
+        showEyes            = true;
+        showPupils          = true;
         
-        crossClr
-        eyeClr
-        pupilClr
+        crossClr            = [255 0 0];
+        eyeClr              = 255;
+        pupilClr            = 0;
         
         referencePos;
-        allPosOff = [0 0];
+        allPosOff           = [0 0];
     end
     
     properties (SetAccess=private)
@@ -57,12 +67,17 @@ classdef ETHead < handle
     
     methods
         function this = ETHead(wpnt,trackBoxHalfWidth,trackBoxHalfHeight)
-            this.wpnt                   = wpnt;
-            this.qFloatColorRange       = Screen('ColorRange',this.wpnt)==1;
-            this.trackBoxHalfWidth      = trackBoxHalfWidth;
-            this.trackBoxHalfHeight     = trackBoxHalfHeight;
-            
-            this.circVerts              = genCircle(200);
+            if nargin==0
+                % run demo
+                ETHead.showDemo();
+            else
+                this.wpnt                   = wpnt;
+                this.qFloatColorRange       = Screen('ColorRange',this.wpnt)==1;
+                this.trackBoxHalfWidth      = trackBoxHalfWidth;
+                this.trackBoxHalfHeight     = trackBoxHalfHeight;
+                
+                this.circVerts              = genCircle(200);
+            end
         end
         
         function update(this,...
@@ -198,6 +213,175 @@ classdef ETHead < handle
                     end
                 end
             end
+        end
+    end
+    
+    methods (Static)
+        function showDemo()
+            DEBUGlevel = 0;
+            
+            scr = max(Screen('Screens'));
+            if DEBUGlevel>1
+                % make screen partially transparent on OSX and windows vista or
+                % higher, so we can debug.
+                PsychDebugWindowConfiguration;
+            end
+            Screen('Preference', 'SyncTestSettings', 0.002);    % the systems are a little noisy, give the test a little more leeway
+            [wpnt,winRect] = PsychImaging('OpenWindow', scr, 127, [], [], [], [], 4);
+            hz=Screen('NominalFrameRate', wpnt);
+            Priority(1);
+            Screen('BlendFunction', wpnt, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            Screen('Preference', 'TextAlphaBlending', 1);
+            Screen('Preference', 'TextAntiAliasing', 2);
+            % This preference setting selects the high quality text renderer on
+            % each operating system: It is not really needed, as the high quality
+            % renderer is the default on all operating systems, so this is more of
+            % a "better safe than sorry" setting.
+            Screen('Preference', 'TextRenderer', 1);
+            Screen('TextSize',wpnt, 40);
+            KbName('UnifyKeyNames');    % for correct operation of the setup/calibration interface, calling this is required
+            
+            % make ETHead
+            head                = ETHead(wpnt,[],[]);
+            head.rectWH         = winRect(3:4);
+            head.refSz          = .1*winRect(3);
+            head.referencePos   = [0 0 65]; % cm, note that position inputs to head.update are in mm, not cm
+            
+            % starting screen
+            DrawFormattedText(wpnt,'This demo will show the below head animated in various ways. Press any key to continue to the next animation.','center',winRect(4)*.15,0,50);
+            head.update(true, [0 0 650].', [], 5, true, [0 0 650].', [], 5);
+            head.draw();
+            Screen('Flip',wpnt);
+            KbStrokeWait;
+            
+            % back and forth in depth, blinking eye
+            range = [500 800];
+            cps = 2/3;
+            dphi= cps*2*pi;
+            dt  = 1/hz;
+            t   = 0;
+            while true
+                DrawFormattedText(wpnt,'Head moving back and forth in depth, eyes blinking.','center',winRect(4)*.15,0,50);
+                normOff = sin(t*dphi);
+                d = range(1) + diff(range)*(.5+normOff/2);
+                head.update(normOff<=0, [0 0 d].', [], 5, normOff>0, [0 0 d].', [], 5);
+                head.draw();
+                Screen('Flip',wpnt);
+                if KbCheck()
+                    break;
+                end
+                t = t+dt;
+            end
+            KbWait([],1);
+            
+            % sideways around rotation point
+            Rdist = 250;
+            range = 15/180*pi;
+            cps = 2/3;
+            dphi= cps*2*pi;
+            dt  = 1/hz;
+            t   = 0;
+            eyesPos = [-30 30;Rdist Rdist];
+            while true
+                DrawFormattedText(wpnt,'Head swinging left-to-right.','center',winRect(4)*.15,0,50);
+                ori = range*sin(t*dphi);
+                Rmat= [cos(ori) sin(ori); -sin(ori) cos(ori)];
+                eyes= Rmat*eyesPos;
+                eyes(2,:) = eyes(2,:)-Rdist;
+                head.update(true, [eyes(:,1); 650], [], 5, true, [eyes(:,2); 650], [], 5);
+                head.draw();
+                Screen('Flip',wpnt);
+                if KbCheck()
+                    break;
+                end
+                t = t+dt;
+            end
+            KbWait([],1);
+            
+            % head yaw
+            Rdist = 60;
+            range = 35/180*pi;
+            cps = 2/3;
+            dphi= cps*2*pi;
+            dt  = 1/hz;
+            t   = 0;
+            eyesPos = [-30 30;Rdist Rdist];
+            while true
+                DrawFormattedText(wpnt,'Head yaw.','center',winRect(4)*.15,0,50);
+                ori = range*sin(t*dphi);
+                Rmat= [cos(ori) sin(ori); -sin(ori) cos(ori)];
+                eyes= Rmat*eyesPos;
+                eyes(2,:) = eyes(2,:)-Rdist;
+                head.update(true, [eyes(1,1) 0 650-eyes(2,1)].', [], 5, true, [eyes(1,2) 0 650-eyes(2,2)].', [], 5);
+                head.draw();
+                Screen('Flip',wpnt);
+                if KbCheck()
+                    break;
+                end
+                t = t+dt;
+            end
+            KbWait([],1);
+            
+            % pupils
+            range = 2.5;
+            cps = 2/3;
+            dphi= cps*2*pi;
+            dt  = 1/hz;
+            t   = 0;  
+            while true
+                DrawFormattedText(wpnt,'Crazy pupils.','center',winRect(4)*.15,0,50);
+                offset = range*sin(t*dphi);
+                head.update(true, [0 0 650].', [], 5+offset, true, [0 0 650].', [], 5-offset);
+                head.draw();
+                Screen('Flip',wpnt);
+                if KbCheck()
+                    break;
+                end
+                t = t+dt;
+            end
+            KbWait([],1);
+            
+            % all together now
+            Rdist1 = 250;
+            range1 = 15/180*pi;
+            Rdist2 = 60;
+            range2 = 35/180*pi;
+            rangep = 1.5;
+            cps = 2/3;
+            dphi= cps*2*pi;
+            dt  = 1/hz;
+            t   = 0;
+            eyesPos1 = [-30 30;Rdist1 Rdist1];
+            eyesPos2 = [-30 30;Rdist2 Rdist2];
+            mode = 0;
+            while true
+                DrawFormattedText(wpnt,'All together now.','center',winRect(4)*.15,0,50);
+                normOff = sin(t*dphi);
+                Rmat1= [cos(range1*normOff) sin(range1*normOff); -sin(range1*normOff) cos(range1*normOff)];
+                eyes1= Rmat1*eyesPos1;
+                eyes1(2,:) = eyes1(2,:)-Rdist1;
+                Rmat2= [cos(range2*normOff) sin(range2*normOff); -sin(range2*normOff) cos(range2*normOff)];
+                if mode
+                    Rmat2 = Rmat2';
+                end
+                eyes2= Rmat2*eyesPos2;
+                eyes2(2,:) = eyes2(2,:)-Rdist2;
+                
+                head.update(true, [(eyes1(1,1)+eyes2(1,1))/2 eyes1(2,1) 650-eyes2(2,1)].', [], 5+rangep*normOff, true, [(eyes1(1,2)+eyes2(1,2))/2 eyes1(2,2) 650-eyes2(2,2)].', [], 5-rangep*normOff);
+                head.draw();
+                Screen('Flip',wpnt);
+                if KbCheck()
+                    mode=mode+1;
+                    head.crossEye = 1;
+                    KbWait([],1);
+                    if mode==2
+                        break;
+                    end
+                end
+                t = t+dt;
+            end
+            
+            sca
         end
     end
     
