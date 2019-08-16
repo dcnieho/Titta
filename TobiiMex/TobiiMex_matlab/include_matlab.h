1055913f2ab55c512782b14c3300aa275adbc0fe
Diederick Niehorster <dcnieho@gmail.com>
2019-08-16 22:34:40 +0200

    fixed up check for windows64 or linux in include matlab header

--- TobiiMex/TobiiMex_matlab/include_matlab.h
+++ TobiiMex/TobiiMex_matlab/include_matlab.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef _WIN64 || defined(__linux__)
+#if defined(_WIN64) || defined(__linux__)
 // for 64bit build, at least when using R2019a, we need to make sure to request using the old API so mex file works with older matlab versions (we support back to R2015b)
 #	define MATLAB_MEXCMD_RELEASE R2017b    // ensure using the 700 API, so mex file also works on the older matlab versions we support
 #	define MW_NEEDS_VERSION_H	// looks like a bug in R2018b onwards, don't know how to check if this is R2018b, define for now
