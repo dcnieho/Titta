bcb14eee2a09421a3b46d5bcd37c5e710ae04396
Diederick Niehorster <dcnieho@gmail.com>
2019-08-23 15:55:57 +0200

    adapt makeTobiiMex to work with GCC 7.4.0 on Linux as well

--- TobiiMex/makeTobiiMex.m
+++ TobiiMex/makeTobiiMex.m
@@ -4,8 +4,8 @@ clc
 myDir = fileparts(mfilename('fullpath'));
 cd(myDir);
 
-isWin    = streq(computer,'PCWIN') || streq(computer,'PCWIN64') || ~isempty(strfind(computer, 'mingw32')); %#ok<STREMP>
-isLinux  = streq(computer,'GLNX86') || streq(computer,'GLNXA64') || ~isempty(strfind(computer, 'linux-gnu')); %#ok<STREMP>
+isWin    = strcmp(computer,'PCWIN') || strcmp(computer,'PCWIN64') || ~isempty(strfind(computer, 'mingw32')); %#ok<STREMP>
+isLinux  = strcmp(computer,'GLNX86') || strcmp(computer,'GLNXA64') || ~isempty(strfind(computer, 'linux-gnu')); %#ok<STREMP>
 isOctave = ismember(exist('OCTAVE_VERSION', 'builtin'), [102, 5]);  % If the built-in variable OCTAVE_VERSION exists, then we are running under GNU/Octave, otherwise not.
 is64Bit = ~isempty(strfind(computer, '64')); %#ok<STREMP>
 
@@ -47,5 +47,5 @@ else
     else
         error('Support for 32bit MATLAB on Linux not planned. May actually just work, go ahead and try');
     end
-    mex('-R2017b', '-v', '-O', 'CXXFLAGS="$CXXFLAGS -std=c++2a"', '-outdir', fullfile(myDir,'TobiiMex_matlab',bitLbl), '-DBUILD_FROM_MEX', sprintf('-L%s',fullfile(myDir,'TobiiMex_matlab',bitLbl)), sprintf('-I%s',fullfile(myDir,'deps','include')), sprintf('-I%s',myDir), sprintf('-I%s',fullfile(myDir,'TobiiMex_matlab')), 'TobiiMex_matlab/TobiiMex_matlab.cpp', 'src/*.cpp', '-ltobii_research');
+    mex('-R2017b', '-v', '-O', 'CXXFLAGS="$CXXFLAGS -std=c++17"', '-outdir', fullfile(myDir,'TobiiMex_matlab',bitLbl), '-DBUILD_FROM_MEX', sprintf('-L%s',fullfile(myDir,'TobiiMex_matlab',bitLbl)), sprintf('-I%s',fullfile(myDir,'deps','include')), sprintf('-I%s',myDir), sprintf('-I%s',fullfile(myDir,'TobiiMex_matlab')), 'TobiiMex_matlab/TobiiMex_matlab.cpp', 'src/*.cpp', '-ltobii_research');
 end
