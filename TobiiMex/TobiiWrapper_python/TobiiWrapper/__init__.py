24a8da219b63cd1b2a6d547a700962139034450f
Diederick Niehorster <dcnieho@gmail.com>
2019-12-13 08:40:18 +0100

    mark debug switch as implementation detail

--- TobiiMex/TobiiWrapper_python/TobiiWrapper/__init__.py
+++ TobiiMex/TobiiWrapper_python/TobiiWrapper/__init__.py
@@ -1,14 +1,14 @@
-﻿debug = False
+﻿_debug = False
 import sys;
 if sys.maxsize > 2**32:
     # running on 64bit platform
-    if debug:
+    if _debug:
         from .x64.TobiiWrapper_python_d import *
     else:
         from .x64.TobiiWrapper_python import *
 else:
     # running on 32bit platform
-    if debug:
+    if _debug:
         from .x86.TobiiWrapper_python_d import *
     else:
         from .x86.TobiiWrapper_python import *
