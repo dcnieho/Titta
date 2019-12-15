3642b4e867b5439cdbc5da928ebe2bf095c08987
Diederick Niehorster <dcnieho@gmail.com>
2019-12-15 21:21:39 +0100

    drawtext function for antisaccade now also works when PTB's text drawer plugin is not available

--- demos/antiSaccade/functions/drawtext.m
+++ demos/antiSaccade/functions/drawtext.m
@@ -1,12 +1,31 @@
 function swaptime = drawtext(text, textSetup, w, flipWhen, qCenterEachLine)
-extra = {};
-if nargin>4 && ~isempty(qCenterEachLine) && qCenterEachLine
-    extra = [extra {'xlayout', 'center'}];
+if nargin<5 || ~isempty(qCenterEachLine)
+    qCenterEachLine = false;
 end
-DrawFormattedText2(text, 'win', w, ...
-    'sx', 'center', 'xalign', 'center', 'sy', 'center', 'yalign', 'center', ...
-    'baseColor', textSetup.color, ...
-    'wrapat', textSetup.wrapAt, ...
-    'vSpacing', textSetup.vSpacing,...
-    extra{:});
+
+% check which text drawer function is being used
+isWin = streq(computer,'PCWIN') || streq(computer,'PCWIN64') || ~isempty(strfind(computer, 'mingw32')); %#ok<*STREMP>
+usingFTGLTextRenderer = (~isWin || ~~exist('libptbdrawtext_ftgl64.dll','file')) && Screen('Preference','TextRenderer')==1;    % check if we're not on Windows, or if on Windows that we the high quality text renderer is used (was never supported for 32bit PTB, so check only for 64bit)
+
+if usingFTGLTextRenderer
+    % this one comes with PsychToolbox
+    extra = {};
+    if qCenterEachLine
+        extra = [extra {'xlayout', 'center'}];
+    end
+    DrawFormattedText2(text, 'win', w, ...
+        'sx', 'center', 'xalign', 'center', 'sy', 'center', 'yalign', 'center', ...
+        'baseColor', textSetup.color, ...
+        'wrapat', textSetup.wrapAt, ...
+        'vSpacing', textSetup.vSpacing,...
+        extra{:});
+else
+    % this one is supplied with Titta
+    if qCenterEachLine
+        xlayout = 'center';
+    else
+        xlayout = 'left';
+    end
+    DrawFormattedText2GDI(w,text,'center','center','center','center',xlayout,textSetup.color,textSetup.wrapAt,textSetup.vSpacing);
+end
 swaptime = Screen('Flip',w,flipWhen);
