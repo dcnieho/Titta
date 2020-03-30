2e78cbfb991c408c7bdcf57232a3e67d53c33bba
Diederick Niehorster <dcnieho@gmail.com>
2020-03-30 14:35:47 +0200

    changed cursorUpdater into a real class

--- function_library/cursorUpdater.m
+++ function_library/cursorUpdater.m
@@ -1,114 +1,115 @@
-function fhndl = cursorUpdater(cursors)
+% This class is part of Titta, a toolbox providing convenient access to
+% eye tracking functionality using Tobii eye trackers
+%
+% Titta can be found at https://github.com/dcnieho/Titta. Check there for
+% the latest version.
+% When using Titta, please cite the following paper:
+%
+% Niehorster, D.C., Andersson, R. & Nystrom, M., (2020). Titta: A toolbox
+% for creating Psychtoolbox and Psychopy experiments with Tobii eye
+% trackers. Behavior Research Methods.
+% doi: https://doi.org/10.3758/s13428-020-01358-8
 
-if nargin<1 || isempty(cursors)
-    fhndl.update        = @(~,~) 1;   % dummy function that swallows arguments and is noop
-    fhndl.updateCursors = @(~,~) 1;
-    fhndl.reset         = @(~,~) 1;
-    return;
-end
-
-usingPoly = isfield(cursors,'poly');
-cursorPolys = [];
-cursorRects = [];
-nAOI        = [];
-nElemPerAOI = [];
-cursorLooks = [];
-cursorIdxs  = [];
-qCursorReset= [];
-cursorReset = [];
-updateCursors(cursors);
-
-currCursor  = nan;
-
-fhndl.update        = @update;
-fhndl.updateCursors = @updateCursors;
-fhndl.reset         = @reset;
-
-    function update(x,y)
-        if usingPoly
-            idx = [];
-            % get in which poly, if any. If polys overlap, first in the
-            % list is used
-            for p=1:nAOI
-                if inPoly([x y],cursorPolys{p})
-                	idx = p;
-                    break;
+classdef cursorUpdater < handle
+    properties
+        usingPoly
+        cursorPolys
+        cursorRects
+        cursorLooks
+        qCursorReset    = true;
+        cursorReset
+        
+        currCursor      = nan;
+    end
+    properties (Access=private, Hidden=true)
+    end
+    
+    methods
+        function this = cursorUpdater(cursors)
+            this.updateCursors(cursors);
+        end
+        
+        function update(this,x,y)
+            if this.usingPoly
+                idx = [];
+                % get in which poly, if any. If polys overlap, first in the
+                % list is used
+                for p=1:this.nAOI
+                    if inPoly([x y],this.cursorPolys{p})
+                        idx = p;
+                        break;
+                    end
+                end
+            else
+                if isempty(this.cursorRects)
+                    idx = [];
+                else
+                    % get in which rect, if any. If rects overlap, first in the list is
+                    % used
+                    idx = find(inRect([x y],this.cursorRects),1);
                 end
             end
-        else
-            if isempty(cursorRects)
-                idx = [];
+            
+            % get corresponding cursor
+            if isempty(idx)
+                curr = this.cursorLooks(end);
             else
-                % get in which rect, if any. If rects overlap, first in the list is
-                % used
-                idx = find(inRect([x y],cursorRects),1);
+                curr = this.cursorLooks(idx);
+            end
+            
+            % see if need to change
+            if this.currCursor ~= curr
+                if curr==-1
+                    HideCursor();
+                else
+                    ShowCursor(curr);
+                end
+                this.currCursor = curr;
             end
         end
         
-        % get corresponding cursor
-        if isempty(idx)
-            curr = cursorLooks(end);
-        else
-            curr = cursorLooks(cursorIdxs(idx));
-        end
-        
-        % see if need to change
-        if currCursor ~= curr
+        function reset(this)
+            if ~this.qCursorReset
+                return;
+            end
+            
+            if ~isempty(this.cursorReset)
+                curr = this.cursorReset;
+            else
+                curr = this.cursorLooks(end);
+            end
             if curr==-1
                 HideCursor();
             else
                 ShowCursor(curr);
             end
-            currCursor = curr;
         end
     end
-
-    function updateCursors(cursors)
-        % process rects/polys
-        usingPoly = isfield(cursors,'poly');
-        if usingPoly
-            cursorPolys = cursors.poly;
-            nAOI        = length(cursorPolys);
-            nElemPerAOI = ones(1,nAOI);
-        else
-            cursorRects = [cursors.rect{:}];
-            nAOI        = length(cursors.rect);
-            nElemPerAOI = cellfun(@(x) size(x,2),cursors.rect);
-        end
-        % cursor looks are numbered IDs as eaten by ShowMouse. -1 means hide cursor
-        cursorLooks = [cursors.cursor cursors.other];
-        cursorIdxs  = SmartVec(1:nAOI,nElemPerAOI,0);
-        
-        % optional (default on) reset of cursor when calling reset(). Have it as an
-        % option as some function out of the reach of the user always call reset
-        % upon exit, user can here configure if it actually does something
-        qCursorReset = true;
-        if isfield(cursors,'qReset')
-            qCursorReset = cursors.qReset;
-        end
-        % if resetting, indicate what cursor to reset to. if empty, we reset to
-        % cursors.other.
-        cursorReset = [];
-        if isfield(cursors,'reset')
-            cursorReset = cursors.reset;
+    
+    methods (Access=private, Hidden=true)
+        function updateCursors(this,cursors)
+            % process rects/polys
+            this.usingPoly = isfield(cursors,'poly');
+            if this.usingPoly
+                this.cursorPolys = cursors.poly;
+            else
+                this.cursorRects = cursors.rect;
+            end
+            % cursor looks are numbered IDs as eaten by ShowMouse. -1 means hide cursor
+            this.cursorLooks = [cursors.cursor cursors.other];
+            
+            % optional (default on) reset of cursor when calling reset(). Have it as an
+            % option as some function out of the reach of the user always call reset
+            % upon exit, user can here configure if it actually does something
+            if isfield(cursors,'qReset')
+                this.qCursorReset = cursors.qReset;
+            end
+            % if resetting, indicate what cursor to reset to. if empty, we reset to
+            % cursors.other.
+            this.cursorReset = [];
+            if isfield(cursors,'reset')
+                this.cursorReset = cursors.reset;
+            end
         end
     end
-
-    function reset()
-        if ~qCursorReset
-            return;
-        end
-        
-        if ~isempty(cursorReset)
-            curr = cursorReset;
-        else
-            curr = cursorLooks(end);
-        end
-        if curr==-1
-            HideCursor();
-        else
-            ShowCursor(curr);
-        end
-    end
-
 end
