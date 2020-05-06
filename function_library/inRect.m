058e72886431d801a6f454bb3fa5508d9204e04a
Diederick Niehorster <dcnieho@gmail.com>
2020-05-06 14:30:52 +0200

    inRect: robust to empty xy or rect argument

--- function_library/inRect.m
+++ function_library/inRect.m
@@ -3,6 +3,11 @@ function qIn = inRect(xy,rects)
 % check if a single x-y coordinate falls in one or more rects, or if a by
 % of x-y coordinates fall in a single rect
 
+if isempty(xy) || isempty(rects)
+    qIn = logical([]);
+    return;
+end
+
 if numel(xy)==2
     xy = xy(:);
 end
