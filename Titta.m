a588d1d0f7c834c5748b4da343ac9bd796cf1f2b
Diederick Niehorster <dcnieho@gmail.com>
2020-05-25 09:05:16 +0200

    add logging when calibration is loaded, and when load causes a change in the binocular/monocular ET mode

--- Titta.m
+++ Titta.m
@@ -825,7 +825,7 @@ classdef Titta < handle
                     obj.doEnterCalibrationMode();
                     qHasEnteredCalMode = true;
                 end
-                obj.loadOtherCal(out.attempt{kCal},[],true);
+                obj.loadOtherCal(out.attempt{kCal},kCal,[],true);
                 currentSelection    = kCal;                 % keeps track of calibration that is currently applied
                 % NB: qNewCal should also in this case be true, as the
                 % setup screen shown first is the start of a potential new
@@ -1120,7 +1120,7 @@ classdef Titta < handle
                 out                 = previousCalibs;
                 % preload the one previously selected by user
                 kCal                = out.selectedCal;      % index into list of calibration attempts
-                obj.loadOtherCal(out.attempt{kCal},[],true);
+                obj.loadOtherCal(out.attempt{kCal},kCal,[],true);
                 currentSelection    = kCal;                 % keeps track of calibration that is currently applied
             else
                 kCal                = 0;                    % index into list of calibration attempts
@@ -3155,7 +3155,7 @@ classdef Titta < handle
                 % viewer
                 % select and load last successful calibration
                 selection = iValid(end);
-                obj.loadOtherCal(cal{selection},true,true);
+                obj.loadOtherCal(cal{selection},selection,true,true);
             end
             qHasCal                 = ~isempty(cal{selection}.cal.result);
             qHaveMultipleValidVals  = ~isempty(iValid) && ~isscalar(iValid);
@@ -3313,7 +3313,7 @@ classdef Titta < handle
                     qUpdateNow = false;
                     if qSelectedCalChanged
                         % load requested cal
-                        obj.loadOtherCal(cal{newSelection},true,true);
+                        obj.loadOtherCal(cal{newSelection},newSelection,true,true);
                         qSelectedCalChanged = false;
                         qAwaitingCalChange = true;
                     else
@@ -5730,10 +5730,12 @@ classdef Titta < handle
             end
         end
         
-        function loadOtherCal(obj,cal,skipCheck,alsoSwitchMode)
-            if ~isempty(cal.cal.computedCal)    % empty when doing zero-point calibration. There is nothing to laod or change then anyway, so is ok to skip. NB: rethink if user ever gains interface for changing number of calibration points
+        function loadOtherCal(obj,cal,calNo,skipCheck,alsoSwitchMode)
+            if ~isempty(cal.cal.computedCal)    % empty when doing zero-point calibration. There is nothing to load or change then anyway, so is ok to skip. NB: rethink if user ever gains interface for changing number of calibration points
+                obj.sendMessage(sprintf('LOAD CALIBRATION (%s), calibration no. %d',getEyeLbl(cal.eye),calNo));
+                
                 % first check we're in right calibration mode
-                if nargin>3 && ~isempty(alsoSwitchMode) && alsoSwitchMode
+                if nargin>4 && ~isempty(alsoSwitchMode) && alsoSwitchMode
                     % check that current calibration mode matches that
                     % of the calibration selected to be loaded.
                     % If not exit, calibration mode and reenter the
@@ -5741,6 +5743,7 @@ classdef Titta < handle
                     if ~strcmp(cal.eye,obj.settings.calibrateEye)
                         % first change eye to be calibrated
                         obj.changeAndCheckCalibEyeMode(cal.eye);
+                        obj.sendMessage(sprintf('CHANGE SETUP to %s',getEyeLbl(obj.settings.calibrateEye)));
                         % now also change mode in all cases (as this
                         % also wipes state clean, so also wanted when
                         % we remain monocular, but switch from left to
@@ -5753,7 +5756,7 @@ classdef Titta < handle
                 % load previous calibration
                 obj.buffer.calibrationApplyData(cal.cal.computedCal);
                 
-                if nargin>2 && ~isempty(skipCheck) && skipCheck
+                if nargin>3 && ~isempty(skipCheck) && skipCheck
                     % return immediately
                     return
                 end
