83cde9415840b80f0fd5a64d61007f8505e3fc2c
Diederick Niehorster <dcnieho@gmail.com>
2020-12-08 14:15:19 +0100

    addTittaToPath: old path should be regexpescape()d before using as pattern

--- addTittaToPath.m
+++ addTittaToPath.m
@@ -1,22 +1,23 @@
 function addTittaToPath()
 % adds titta to path, ignoring at least some of the unneeded folders
-mpath			= path;
-mpath			= strsplit(mpath, pathsep);
-opath			= fileparts(mfilename('fullpath'));
+mpath           = path;
+mpath           = strsplit(mpath, pathsep);
+opath           = fileparts(mfilename('fullpath'));
+opathesc        = regexptranslate('escape',opath);
 for i = 1:length(mpath)
-	if ~isempty(regexpi(mpath{i},opath))
-		rmpath(mpath{i}); % remove any old path values
-	end
+    if ~isempty(regexpi(mpath{i},opathesc))
+        rmpath(mpath{i}); % remove any old path values
+    end
 end
-opaths			= genpath(opath); 
-opaths			= strsplit(opaths,pathsep);
-newpaths		= {};
+opaths          = genpath(opath);
+opaths          = strsplit(opaths,pathsep);
+newpaths        = {};
 sep             = regexptranslate('escape',filesep);
 pathExceptions  = [sep '\.git|' sep 'deps'];
 for i=1:length(opaths)
-	if isempty(regexpi(opaths{i},pathExceptions))
-		newpaths{end+1}=opaths{i};
-	end
+    if isempty(regexpi(opaths{i},pathExceptions))
+        newpaths{end+1}=opaths{i};
+    end
 end
 newpaths = strjoin(newpaths,pathsep);
 addpath(newpaths); savepath;
