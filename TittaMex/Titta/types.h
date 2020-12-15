0531100dffc1983f2f6adce60c586d2c6735e148
Diederick Niehorster <dcnieho@gmail.com>
2020-12-15 22:10:59 +0100

    std::shared_timed_mutex -> std::shared_mutex, since we don't need the timed locking fuctionality

--- TittaMex/Titta/types.h
+++ TittaMex/Titta/types.h
@@ -11,7 +11,7 @@
 #include <tobii_research_streams.h>
 #include <tobii_research_calibration.h>
 
-using mutex_type = std::shared_timed_mutex;
+using mutex_type = std::shared_mutex;
 using read_lock = std::shared_lock<mutex_type>;
 using write_lock = std::unique_lock<mutex_type>;
 
