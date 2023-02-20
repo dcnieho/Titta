ce63c575a6c8241a18b9de51c5c70939a056d42f
Diederick Niehorster <dcnieho@gmail.com>
2023-02-20 09:56:43 +0100

    fix up addTittaToPath yet again so it correctly put only one OS folder on path

--- addTittaToPath.m
+++ addTittaToPath.m
@@ -22,20 +22,20 @@ qAdd            = cellfun(@isempty,regexpi(opaths,pathExceptions)); % true where
 isWin    = strcmp(computer,'PCWIN')                             || strcmp(computer,'PCWIN64') || ~isempty(strfind(computer, 'mingw32')); %#ok<STREMP>
 isLinux  = strcmp(computer,'GLNX86')                            || strcmp(computer,'GLNXA64') || ~isempty(strfind(computer, 'linux-gnu')); %#ok<STREMP>
 isOSX    = strcmp(computer,'MAC')    || strcmp(computer,'MACI') || strcmp(computer, 'MACI64') || ~isempty(strfind(computer, 'apple-darwin')); %#ok<STREMP>
-if isLinux
-    % exclude Windows and OSX mex folders
-    qAdd = qAdd & cellfun(@isempty,strfind(opaths,'TittaMex/64/Windows')); %#ok<STRCLFH>
-    qAdd = qAdd & cellfun(@isempty,strfind(opaths,'TittaMex/64/OSX')); %#ok<STRCLFH>
-elseif isOSX
-    % exclude Windows and Linux mex folders
-    qAdd = qAdd & cellfun(@isempty,strfind(opaths,'TittaMex/64/Windows')); %#ok<STRCLFH>
-    qAdd = qAdd & cellfun(@isempty,strfind(opaths,'TittaMex/64/Linux')); %#ok<STRCLFH>
-elseif isWin
-    % exclude Linux ans OSX mex folders
-    qAdd = qAdd & cellfun(@isempty,strfind(opaths,'TittaMex/64/Linux')); %#ok<STRCLFH>
-    qAdd = qAdd & cellfun(@isempty,strfind(opaths,'TittaMex/64/OSX')); %#ok<STRCLFH>
-else
+if ~isWin && ~isLinux && ~isOSX
     error('unsupported platform')
+end
+if ~isLinux
+    % exclude Linux mex folder
+    qAdd = qAdd & cellfun(@isempty,regexpi(opaths,'TittaMex(\/|\\)64(\/|\\)Linux'));
+end
+if ~isOSX
+    % exclude OSX mex folder
+    qAdd = qAdd & cellfun(@isempty,regexpi(opaths,'TittaMex(\/|\\)64(\/|\\)OSX'));
+end
+if ~isWin
+    % exclude Windows mex folder
+    qAdd = qAdd & cellfun(@isempty,regexpi(opaths,'TittaMex(\/|\\)64(\/|\\)Windows'));
 end
 addpath(opaths{qAdd}); savepath;
 disp('--->>> Added Titta to the path...')
