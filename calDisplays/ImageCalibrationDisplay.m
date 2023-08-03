e6317bae2f9edc3509b290004214e9d74ced3f20
Diederick Niehorster <dcnieho@gmail.com>
2023-08-03 14:34:53 +0200

    add image calibration demo. Not fully finished yet, need different timings for the two animated gifs, and some issue with transparency

---
+++ calDisplays/ImageCalibrationDisplay.m
@@ -0,0 +1,276 @@
+classdef ImageCalibrationDisplay < handle
+    properties (Access=private, Constant)
+        calStateEnum = struct('undefined',0, 'showing',1, 'blinking',2);
+    end
+    properties (Access=private)
+        calState;
+        currentPoint;
+        pointStartT;
+        blinkStartT;
+        texs;
+    end
+    properties (SetAccess=private)
+        images              = {};
+        imageDurations      = [];
+        imageScales         = [];
+    end
+    properties
+        blinkInterval       = 0.3;
+        blinkCount          = 2;
+        bgColor             = 127;
+    end
+    properties (Access=private, Hidden = true)
+        qFloatColorRange;
+        cumDurations;
+        qTexsLoaded;
+    end
+    
+    
+    methods
+        function obj = ImageCalibrationDisplay()
+            obj.setCleanState();
+        end
+        
+        function setCleanState(obj)
+            obj.calState        = obj.calStateEnum.undefined;
+            obj.currentPoint    = nan(1,3);
+            if ~isempty(obj.texs)
+                Screen('Close',[obj.texs.tex]);
+                obj.texs = [];
+            end
+            obj.qTexsLoaded     = false;
+        end
+        
+        function setImages(obj,wpnt,imageFiles,imageDurations,imageScales)
+            % check input
+            imageInfo = obj.interrogateImages(imageFiles);
+
+            if isscalar(imageDurations)
+                imageDurations = repmat(imageDurations,1,length(imageInfo));
+            end
+            assert(numel(imageDurations)==length(imageInfo),'either specify a single duration for all image files, or a duration per image file (or per image frame if files contain multiple frames, like animated gifs)')
+            obj.imageDurations = imageDurations;
+            obj.cumDurations   = cumsum([0 obj.imageDurations]);
+            
+            if nargin<4 || isempty(imageScales)
+                imageScales = ones(1,length(imageInfo));
+            elseif isscalar(imageScales)
+                imageScales = repmat(imageScales,1,length(imageInfo));
+            end
+            assert(numel(imageScales)==length(imageInfo),'either specify a single scale for all image files, or a scale per image file (or per image frame if files contain multiple frames, like animated gifs)')
+            obj.imageScales = imageScales;
+            
+            obj.images = imageInfo;
+            
+            % load images
+            obj.loadImages();
+            if ~isempty(wpnt)
+                obj.uploadImages(wpnt);
+            end
+        end
+        
+        function qAllowAcceptKey = doDraw(obj,wpnt,drawCmd,currentPoint,pos,~,~)
+            % last two inputs, tick (monotonously increasing integer) and
+            % stage ("cal" or "val") are not used in this code
+            
+            % if called with drawCmd == 'cleanUp', this is a signal that
+            % calibration/validation is done, and cleanup can occur if
+            % wanted
+            if strcmp(drawCmd,'cleanUp')
+                obj.setCleanState();
+                return;
+            end
+            
+            % now that we have a wpnt, interrogate window
+            if isempty(obj.qFloatColorRange)
+                obj.qFloatColorRange    = Screen('ColorRange',wpnt)==1;
+            end
+            
+            % delay-load images if needed
+            if ~obj.qTexsLoaded
+                obj.uploadImages(wpnt);
+            end
+            
+            % check point changed
+            curT = GetSecs;     % instead of using time directly, you could use the 'tick' call sequence number input to this function to animate your display
+            if strcmp(drawCmd,'new')
+                obj.currentPoint    = [currentPoint pos];
+                obj.pointStartT     = curT;
+                obj.calState        = obj.calStateEnum.showing;
+            elseif strcmp(drawCmd,'redo')
+                % start blink, restart animation.
+                obj.calState        = obj.calStateEnum.blinking;
+                obj.blinkStartT     = curT;
+                obj.pointStartT     = 0;
+            else % drawCmd == 'draw'
+                % regular draw: check state transition
+                if obj.calState==obj.calStateEnum.blinking && (curT-obj.blinkStartT)>obj.blinkInterval*obj.blinkCount*2
+                    % blink finished
+                    obj.calState    = obj.calStateEnum.showing;
+                    obj.pointStartT = curT;
+                end
+            end
+            
+            % determine current point position
+            curPos = obj.currentPoint(2:3);
+            
+            % determine if we're ready to accept the user pressing the
+            % accept calibration point button. User should not be able to
+            % press it if point is not yet at the final position
+            qAllowAcceptKey = obj.calState~=obj.calStateEnum.blinking;
+            
+            % draw
+            if obj.calState~=obj.calStateEnum.blinking
+                [~,~,whichIm] = histcounts(mod(curT-obj.pointStartT,obj.cumDurations(end)),obj.cumDurations);
+            else
+                whichIm = 1;
+            end
+            Screen('FillRect',wpnt,obj.getColorForWindow(obj.bgColor)); % needed when multi-flipping participant and operator screen, doesn't hurt when not needed
+            if obj.calState~=obj.calStateEnum.blinking || mod((curT-obj.blinkStartT)/obj.blinkInterval/2,1)>.5
+                rect = CenterRectOnPointd(obj.texs(whichIm).scrRect,curPos(1),curPos(2));
+                Screen('DrawTexture',wpnt,obj.texs(whichIm).tex,[],rect);
+            end
+        end
+    end
+    
+    methods (Access = private, Hidden, Static)
+        function imageInfo = interrogateImages(imageFiles)
+            imageInfo = cell2struct(cell(4,1,0),{'file','ext','frameIdx','info'});   % make empty struct
+            for p=1:length(imageFiles)
+                % get info about images
+                [~,~,ext] = fileparts(imageFiles{p});
+                if ext(1)=='.'
+                    ext(1) = [];
+                end
+                if ismember(lower(ext),{'gif','jpg','png','tif','tiff'})
+                    imInfo = imfinfo(imageFiles{p});
+                    nImage = length(imInfo);    % gif and tiff may contain multiple images
+                    for f=1:nImage
+                        imageInfo(1,end+1) = struct('file',imageFiles{p},'ext',ext,'frameIdx',f,'info',[]); %#ok<AGROW>
+                        if ismember(lower(ext),{'tif','tiff'})
+                            imageInfo(1,end).info = imInfo;
+                        elseif strcmpi(ext,'gif')
+                            imageInfo(1,end).info = imInfo(f);
+                        end
+                    end
+                else
+                    error('Files other than gif, jpg, png and tif are not supported')
+                end
+            end
+        end
+
+        function imdata = loadImage(image)
+            switch lower(image.ext)
+                case {'jpg','png'}
+                    assert(frame==1,'Reading more than one frame from jpg or png file is not supported')
+                    [imdata,cmap,a] = imread(image.file);
+                case {'gif'}
+                    [imdata,cmap]   = imread(image.file,image.frameIdx);
+                    % check if gif has a transparent color. If so,
+                    % create alpha layer
+                    % 1. get which color is transparent
+                    tIdx            = nan;
+                    if isfield(image.info,'TransparentColor')
+                        tIdx        = image.info.TransparentColor;
+                    end
+                    % 2. create the alpha layer
+                    if ~isnan(tIdx) && ~isempty(tIdx)
+                        a = imdata ~= tIdx-1;   % indexed image is zero-based, tIdx is 1-based
+                        a = cast(a,'like',imdata) * intmax(class(imdata));
+                    else
+                        a = [];
+                    end
+                case {'tif','tiff'}
+                    [imdata,cmap,a] = imread(image.file,'Index',image.frameIdx,'Info',image.info);
+            end
+
+            % if indexed image, turn into RGB image
+            if ~isempty(cmap)
+                cmap    = permute(cmap,[1 3 2]);
+                imdata  = reshape(cmap(imdata(:)+1,1,:),[size(imdata) 3]);
+            end
+            
+            % convert to double with 0--1 range
+            if isinteger(imdata)
+                imdata = double(imdata)/double(intmax(class(imdata)));
+            end
+            
+            % if single plane (grayscale, expand to three planes)
+            if size(imdata,3)==1
+                imdata = repmat(imdata,1,1,3);
+            end
+            
+            % add alpha channel, if any.
+            % ensure its of right type
+            if isa(imdata,'double') && ~isa(a,'double')
+                a = double(a)/double(intmax(class(a)));
+            end
+            % add
+            imdata          = cat(3,imdata,a);
+        end
+
+        function tex = uploadImage(imdata,wpnt,scrSize,qDontScaleUp)
+            % get some info about image
+            tex.size  = [size(imdata,1) size(imdata,2)];
+            tex.ext   = fliplr(tex.size);   % size is [y x], for rects need [x y]
+
+            % store image data
+            if nargin>2 && ~isempty(wpnt)
+                % upload as PTB texture
+                if Screen('ColorRange',wpnt)==1
+                    tex.tex = Screen('MakeTexture',wpnt,imdata,[],[],1);
+                else
+                    imdata      = uint8(round(imdata*255));
+                    tex.tex = Screen('MakeTexture',wpnt,imdata);
+                end
+            else
+                % simply pass back out
+                tex.imdata = imdata;
+            end
+
+            if nargin>3 && ~isempty(scrSize)
+                % find scale fac to make it fit on screen
+                tex.scaleFac = min(scrSize./tex.ext);
+                if nargin>4 && ~isempty(qDontScaleUp) && qDontScaleUp
+                    % make sure image is not scaled up (scaleFac not higher
+                    % than 1)
+                    tex.scaleFac = min(1, tex.scaleFac);
+                end
+                tex.scrRect = CenterRect([0 0 tex.ext].*min(tex.scaleFac),[0 0 scrSize]);
+            end
+        end
+    end
+    
+    methods (Access = private, Hidden)
+        function loadImages(obj)
+            for p=1:length(obj.images)
+                obj.images(p).imdata = obj.loadImage(obj.images(p));
+            end
+        end
+
+        function uploadImages(obj,wpnt)
+            scrSize = Screen('Rect',wpnt);
+            scrSize(1:2) = [];
+            for p=1:length(obj.images)
+                if ~isfield(obj.images(p),'imdata') || isempty(obj.images(p).imdata)
+                    obj.images(p).imdata = obj.loadImage(obj.images(p));
+                end
+                tex = obj.uploadImage(obj.images(p).imdata,wpnt,scrSize,true);
+                tex.scaleFac = min(obj.imageScales(p), tex.scaleFac);
+                tex.scrRect = CenterRectOnPointd([0 0 tex.ext].*min(tex.scaleFac),0,0);
+                if p==1
+                    obj.texs = tex;
+                else
+                    obj.texs(p) = tex;
+                end
+            end
+            obj.qTexsLoaded = true;
+        end
+        
+        function clr = getColorForWindow(obj,clr)
+            if obj.qFloatColorRange
+                clr = double(clr)/255;
+            end
+        end
+    end
+end
---
+++ demos/readmeImageCal.m
@@ -0,0 +1,217 @@
+% this demo code is part of Titta, a toolbox providing convenient access to
+% eye tracking functionality using Tobii eye trackers
+%
+% Titta can be found at https://github.com/dcnieho/Titta. Check there for
+% the latest version.
+% When using Titta, please cite the following paper:
+%
+% Niehorster, D.C., Andersson, R. & Nystrom, M., (2020). Titta: A toolbox
+% for creating Psychtoolbox and Psychopy experiments with Tobii eye
+% trackers. Behavior Research Methods.
+% doi: https://doi.org/10.3758/s13428-020-01358-8
+
+clear all
+sca
+
+DEBUGlevel              = 0;
+fixClrs                 = [0 255];
+bgClr                   = 127;
+useAnimatedCalibration  = true;
+doBimonocularCalibration= false;
+imageFolder             = fullfile(fileparts(mfilename("fullpath")),'..','calDisplays');
+images                  = cellfun(@(x) fullfile(imageFolder,x),{'colorWheel.gif','flower.gif'},'uni',false);
+imageFrameDurations     = 0.1; % s
+imageScaleFac           = 1;
+% task parameters
+fixTime                 = .5;
+imageTime               = 4;
+scr                     = max(Screen('Screens'));
+
+% You can run addTittaToPath once to "install" it, or you can simply add a
+% call to it in your script so each time you want to use Titta, it is
+% ensured it is on path
+home = cd;
+cd ..;
+addTittaToPath;
+cd(home);
+
+try
+    % get setup struct (can edit that of course):
+    settings = Titta.getDefaults('Tobii Pro Spectrum');
+    % request some debug output to command window, can skip for normal use
+    settings.debugMode      = true;
+    % customize colors of setup and calibration interface (colors of
+    % everything can be set, so there is a lot here).
+    % 1. setup screen
+    settings.UI.setup.bgColor       = bgClr;
+    settings.UI.setup.instruct.color= fixClrs(1);
+    settings.UI.setup.fixBackColor  = fixClrs(1);
+    settings.UI.setup.fixFrontColor = fixClrs(2);
+    % 2. validation result screen
+    settings.UI.val.bgColor                 = bgClr;
+    settings.UI.val.avg.text.color          = fixClrs(1);
+    settings.UI.val.fixBackColor            = fixClrs(1);
+    settings.UI.val.fixFrontColor           = fixClrs(2);
+    settings.UI.val.onlineGaze.fixBackColor = fixClrs(1);
+    settings.UI.val.onlineGaze.fixFrontColor= fixClrs(2);
+    % calibration display
+    % custom calibration drawer
+    calViz                           = ImageCalibrationDisplay();
+    calViz.setImages([],images,imageFrameDurations,imageScaleFac);
+    calViz.bgColor                   = bgClr;
+    settings.cal.drawFunction        = @calViz.doDraw;
+    % NB: initialized further below
+    % callback function for completion of each calibration point
+    settings.cal.pointNotifyFunction = @demoCalCompletionFun;
+    
+    % init
+    EThndl          = Titta(settings);
+    % EThndl          = EThndl.setDummyMode();    % just for internal testing, enabling dummy mode for this readme makes little sense as a demo
+    EThndl.init();
+    
+    if DEBUGlevel>1
+        % make screen partially transparent on OSX and windows vista or
+        % higher, so we can debug.
+        PsychDebugWindowConfiguration;
+    end
+    if DEBUGlevel
+        % Be pretty verbose about information and hints to optimize your code and system.
+        Screen('Preference', 'Verbosity', 4);
+    else
+        % Only output critical errors and warnings.
+        Screen('Preference', 'Verbosity', 2);
+    end
+    Screen('Preference', 'SyncTestSettings', 0.002);    % the systems are a little noisy, give the test a little more leeway
+    [wpnt,winRect] = PsychImaging('OpenWindow', scr, bgClr, [], [], [], [], 4);
+    hz=Screen('NominalFrameRate', wpnt);
+    Priority(1);
+    Screen('BlendFunction', wpnt, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    Screen('Preference', 'TextAlphaBlending', 1);
+    Screen('Preference', 'TextAntiAliasing', 2);
+    % This preference setting selects the high quality text renderer on
+    % each operating system: It is not really needed, as the high quality
+    % renderer is the default on all operating systems, so this is more of
+    % a "better safe than sorry" setting.
+    Screen('Preference', 'TextRenderer', 1);
+    KbName('UnifyKeyNames');    % for correct operation of the setup/calibration interface, calling this is required
+    
+    % do calibration
+    try
+        ListenChar(-1);
+    catch ME
+        % old PTBs don't have mode -1, use 2 instead which also supresses
+        % keypresses from leaking through to matlab
+        ListenChar(2);
+    end
+    if doBimonocularCalibration
+        % do sequential monocular calibrations for the two eyes
+        settings                = EThndl.getOptions();
+        settings.calibrateEye   = 'left';
+        settings.UI.button.setup.cal.string = 'calibrate left eye (<i>spacebar<i>)';
+        str = settings.UI.button.val.continue.string;
+        settings.UI.button.val.continue.string = 'calibrate other eye (<i>spacebar<i>)';
+        EThndl.setOptions(settings);
+        tobii.calVal{1}         = EThndl.calibrate(wpnt,1);
+        if ~tobii.calVal{1}.wasSkipped
+            settings.calibrateEye   = 'right';
+            settings.UI.button.setup.cal.string = 'calibrate right eye (<i>spacebar<i>)';
+            settings.UI.button.val.continue.string = str;
+            EThndl.setOptions(settings);
+            tobii.calVal{2}         = EThndl.calibrate(wpnt,2);
+        end
+    else
+        % do binocular calibration
+        tobii.calVal{1}         = EThndl.calibrate(wpnt);
+    end
+    ListenChar(0);
+    
+    
+    % later:
+    EThndl.buffer.start('gaze');
+    WaitSecs(.8);   % wait for eye tracker to start and gaze to be picked up
+    
+    % send message into ET data file
+    EThndl.sendMessage('test');
+    
+    % First draw a fixation point
+    Screen('gluDisk',wpnt,fixClrs(1),winRect(3)/2,winRect(4)/2,round(winRect(3)/100));
+    startT = Screen('Flip',wpnt);
+    % log when fixation dot appeared in eye-tracker time. NB:
+    % system_timestamp of the Tobii data uses the same clock as
+    % PsychToolbox, so startT as returned by Screen('Flip') can be used
+    % directly to segment eye tracking data
+    EThndl.sendMessage('FIX ON',startT);
+    
+    % read in konijntjes image (may want to preload this before the trial
+    % to ensure good timing)
+    stimFName   = 'konijntjes1024x768.jpg';
+    stimDir     = fullfile(PsychtoolboxRoot,'PsychDemos');
+    stimFullName= fullfile(stimDir,stimFName);
+    im          = imread(stimFullName);
+    tex         = Screen('MakeTexture',wpnt,im);
+    
+    % show on screen and log when it was shown in eye-tracker time.
+    % NB: by setting a deadline for the flip, we ensure that the previous
+    % screen (fixation point) stays visible for the indicated amount of
+    % time. See PsychToolbox demos for further elaboration on this way of
+    % timing your script.
+    Screen('DrawTexture',wpnt,tex);                     % draw centered on the screen
+    imgT = Screen('Flip',wpnt,startT+fixTime-1/hz/2);   % bit of slack to make sure requested presentation time can be achieved
+    EThndl.sendMessage(sprintf('STIM ON: %s',stimFName),imgT);
+    
+    % record x seconds of data, then clear screen. Indicate stimulus
+    % removed, clean up
+    endT = Screen('Flip',wpnt,imgT+imageTime-1/hz/2);
+    EThndl.sendMessage(sprintf('STIM OFF: %s',stimFName),endT);
+    Screen('Close',tex);
+    
+    % slightly less precise ISI is fine..., about 1s give or take a frame
+    % or continue immediately if the above upload actions took longer than
+    % a second.
+    WaitSecs(1-(GetSecs-endT));
+    
+    % repeat the above but show a different image. lets also record some
+    % eye images, if supported on connected eye tracker
+    if EThndl.buffer.hasStream('eyeImage')
+       EThndl.buffer.start('eyeImage');
+    end
+    % 1. fixation point
+    Screen('gluDisk',wpnt,fixClrs(1),winRect(3)/2,winRect(4)/2,round(winRect(3)/100));
+    startT = Screen('Flip',wpnt);
+    EThndl.sendMessage('FIX ON',startT);
+    % 2. image
+    stimFNameBlur   = 'konijntjes1024x768blur.jpg';
+    stimFullNameBlur= fullfile(stimDir,stimFNameBlur);
+    im              = imread(stimFullNameBlur);
+    tex             = Screen('MakeTexture',wpnt,im);
+    Screen('DrawTexture',wpnt,tex);                     % draw centered on the screen
+    imgT = Screen('Flip',wpnt,startT+fixTime-1/hz/2);   % bit of slack to make sure requested presentation time can be achieved
+    EThndl.sendMessage(sprintf('STIM ON: %s',stimFNameBlur),imgT);
+    
+    % 4. end recording after x seconds of data again, clear screen.
+    endT = Screen('Flip',wpnt,imgT+imageTime-1/hz/2);
+    EThndl.sendMessage(sprintf('STIM OFF: %s',stimFNameBlur),endT);
+    Screen('Close',tex);
+    
+    % stop recording
+    if EThndl.buffer.hasStream('eyeImage')
+        EThndl.buffer.stop('eyeImage');
+    end
+    EThndl.buffer.stop('gaze');
+    
+    % save data to mat file, adding info about the experiment
+    dat = EThndl.collectSessionData();
+    dat.expt.winRect = winRect;
+    dat.expt.stimDir = stimDir;
+    save(EThndl.getFileName(fullfile(cd,'t'), true),'-struct','dat');
+    % NB: if you don't want to add anything to the saved data, you can use
+    % EThndl.saveData directly
+    
+    % shut down
+    EThndl.deInit();
+catch me
+    sca
+    ListenChar(0);
+    rethrow(me)
+end
+sca
