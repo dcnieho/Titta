a928a01264fac338be3597e550188f76731b6fdb
Diederick Niehorster <dcnieho@gmail.com>
2023-08-09 14:20:55 +0200

    fixed up final step of AOI analysis

--- demo_analysis/d_AOIfix.m
+++ demo_analysis/d_AOIfix.m
@@ -1,80 +1,56 @@
-%*****************************************************************
-%*****************************************************************
-
-clear variables; clear mex; close all; fclose('all'); clc
+clear variables; clear global; clear mex; close all; fclose('all'); clc
 
 dbstop if error % for debugging: trigger a debug point when an error occurs
 
+% setup directories
+myDir = fileparts(mfilename('fullpath'));
+cd(myDir);
                                 dirs.home       = cd;
 cd data;                        dirs.data       = cd;
-        cd mat;                 dirs.mat        = cd;
-        cd ..;
-        cd evt;                 dirs.msgs       = cd;
-        cd ..;
-        cd fix;                 dirs.fix        = cd;
-        cd ..;
+        cd samples_ophak;       dirs.samples    = cd;
+cd ..;  cd fixDet;              dirs.fix        = cd;
+cd ..;
+if ~isdir('AOIfix') %#ok<*ISDIR>
+    mkdir(fullfile(cd,'AOIfix'));
+end
         cd AOIfix;              dirs.AOIfix     = cd;
-        cd ..;
-        cd samples;             dirs.samples    = cd;
-        cd ..;
-        cd samples_ophak;       dirs.samplesO   = cd;
-        cd ..;
+cd ..;  cd msgs_ophak;          dirs.msgsO      = cd;
+cd ..;  cd mat;                 dirs.mat        = cd;
+cd ..;
 cd ..;
 cd function_library;            dirs.funclib    = cd;
 cd ..;
-cd results;                     dirs.results    = cd;
+cd AOIs;                        dirs.AOIs       = cd;
 cd ..;
-cd stimuli;                     dirs.stim       = cd;
-        cd images;              dirs.im         = cd;
-        cd ..;
-        cd AOIs;                dirs.AOIim      = cd;
+cd results;                     dirs.res        = cd;
 cd(dirs.home);
-
-fs = filesep;
-addpath(genpath(dirs.funclib));
+addpath(genpath(dirs.funclib));                 % add dirs to path
 
 %*****************************************************************
-%*****************************************************************
-
-% settings
-
-whichTasks = {
-    'practice',  'screen/(.+): practiceStim.%d$'
-    'test',      'screen/(.+): testStim.%d$'
-    };
-
 %*****************************************************************
 
 %%% get all trials, parse into subject and stimulus
 [files,nfiles] = FileFromFolder(dirs.fix,[],'mat');
+files           = parseFileNames(files);
 
 if 0
     % filter so we only get data that matches the filter. uses regexp
-    filtstr = 'S07';
+    filtstr = '^(?!01|02|03).*$';
     results = regexpi({files.name}.',filtstr,'start');
     files   = files(~cellfun(@isempty,results));
     nfiles  = length(files);
 end
 
 % load all AOIs
-AOI     = loadAllAOIFolders(dirs.AOIim,'png');    % AOI is struct met alle gegevens van de AOI masks
+AOI     = loadAllAOIFolders(dirs.AOIs,'png');    % AOI is struct met alle gegevens van de AOI masks
 AOInms  = {AOI.name};
 
 % per subject, per trial, read data and see which AOIs fixations are in, if
-% any. 0 is other, -1 is out of screen
+% any. 0 is other (no AOI), -1 is not on stimulus, -2 is out of screen
+lastRead= '';
 for p=1:nfiles
     disp(files(p).fname)
     
-    fs      = strsplit(files(p).name,' Samples_');
-    baseFile= fs{1};
-    msgFile = [baseFile ' Samples.txt'];
-    fs      = strsplit(files(p).name,'_');
-    task    = fs{end-1};
-    qTask   = strcmp(task,whichTasks(:,1));
-    if ~any(qTask)
-        continue;
-    end
-    run     = sscanf(fs{end},'R%f.txt');
     % load fix data
     dat     = load(fullfile(dirs.fix,[files(p).fname '.mat'])); dat = dat.dat;
     
@@ -83,20 +59,23 @@ for p=1:nfiles
         continue;
     end
     
+    % get msgs
+    msgs    = loadMsgs(fullfile(dirs.msgsO,[files(p).fname '.txt']));
+    [times,what,~] = parseMsgs(msgs);
+
+    sessionFileName = sprintf('%s.mat',files(p).subj);
+    if ~strcmp(lastRead,sessionFileName)
+        sess = load(fullfile(dirs.mat,sessionFileName),'expt');
+        lastRead = sessionFileName;
+        fInfo = [sess.expt.stim.fInfo];
+    end
+    qWhich= strcmp({fInfo.name},what{1});
+    assert(sum(qWhich)==1,'No or too many presentation info (texs field) found for this stimulus')
+    
     % get more info about stimulus shown etc
-    mat     = load(fullfile(dirs.mat,baseFile),'screens','texs','expt');
-    
-    msgs    = cellfun(@(x) x.IDmsg,mat.screens,'uni',false);
-    matchStr= sprintf(whichTasks{qTask,2},run);
-    iWhich  = find(~cellfun(@isempty,regexp(msgs,matchStr)));
-    stim    = regexp(msgs{iWhich},matchStr,'tokens','once');
-    stim    = stim{1};
-    
-    qTex    = arrayfun(@(x) strcmp(x.fInfo.name,stim),mat.texs);
-    assert(sum(qTex)==1,'No or too many presentation info (texs field) found for this stimulus')
-    tex     = mat.texs(qTex);
-    qAOI    = strcmp(stim,AOInms);
-    assert(sum(qAOI)==1,'No or too many AOIs lists found for this stimulus: %s',stim)
+    tex     = sess.expt.stim(qWhich);
+    qAOI    = strcmp(what{1},AOInms);
+    assert(sum(qAOI)==1,'No or too many AOIs lists found for this stimulus: %s',what{1})
     
     % throw out fixations that onset before first stimulus shown
     qDel = dat.fix.startT<=0;
@@ -107,30 +86,30 @@ for p=1:nfiles
         end
     end
     
-    % check sizes are correct, i.e., AOI boolean images macth in size with
+    % check sizes are correct, i.e., AOI boolean images match in size with
     % shown images
     AOIbools    = {AOI(qAOI).AOIs.bool};
     szs         = cellfun(@size,AOIbools,'uni',false);
     assert(isequal(tex.size,szs{:}),'Some AOIs have wrong size (doesn''t match stimulus)');
     
     % see which AOIs fixations are in
-    temp    = bepaalAOIfixaties3(AOI(qAOI).AOIs,dat.fix.xpos,dat.fix.ypos,mat.expt.scr.res,tex.scrRect,1./tex.scaleFac);
+    temp    = detAOIfix(AOI(qAOI).AOIs,dat.fix.xpos,dat.fix.ypos,sess.expt.winRect(3:4),tex.scrRect,1./tex.scaleFac);
     
     % use fixation ID to find corresponding info about the fixations.
     % This as one fixation can be in multiple AOIs
-    fixAOI          = cell(size(temp,1),13);
-    fixAOI(:,4)     = temp(:,1);
-    fixAOI(:,12:13) = temp(:,2:3);
+    fixAOI          = cell(size(temp,1),11);
+    fixAOI(:,2)     = temp(:,1);    % fixation sequence number
+    fixAOI(:,10:11) = temp(:,2:3);  % AOI sequence number and name
     for r=1:size(fixAOI,1)
-        fnr = fixAOI{r,4};
-        fixAOI(r,[1:3 5:11]) = {task,run,stim,dat.fix.startT(fnr)/1000,dat.fix.dur(fnr)/1000,dat.fix.xpos(fnr),dat.fix.ypos(fnr),dat.fix.RMSxy(fnr),dat.fix.BCEA(fnr),dat.fix.fracinterped(fnr)*100};
+        fnr = fixAOI{r,2};
+        fixAOI(r,[1 3:9]) = {what{1},dat.fix.startT(fnr)/1000,dat.fix.dur(fnr)/1000,dat.fix.xpos(fnr),dat.fix.ypos(fnr),dat.fix.RMSxy(fnr),dat.fix.BCEA(fnr),dat.fix.fracinterped(fnr)*100};
     end
     
     % open file, write data
-    fid = fopen(fullfile(dirs.AOIfix,[files(p).fname '.txt']),'wt');
-    fprintf(fid,'phase\tsequence nr\tstimulus name\tfixNr\tstartT\tduration\tX (pix)\tY (pix)\tRMS\tBCEA\tdata loss (%%)\tAOI nr\tAOI name\n');
+    fid = fopen(fullfile(dirs.AOIfix,[files(p).fname '.tsv']),'wt');
+    fprintf(fid,'stimulus name\tfixNr\tstartT\tduration\tX (pix)\tY (pix)\tRMS\tBCEA\tdata loss (%%)\tAOI nr\tAOI name\n');
     schrijfdata = fixAOI.';
-    fprintf(fid,'%s\t%d\t%s\t%d\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.1f\t%d\t%s\n',schrijfdata{:});
+    fprintf(fid,'%s\t%d\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.1f\t%d\t%s\n',schrijfdata{:});
     fclose(fid);
 end
 
--- demo_analysis/function_library/AOI/detAOIfix.m
+++ demo_analysis/function_library/AOI/detAOIfix.m
@@ -1,7 +1,7 @@
-function [fixAOI] = bepaalAOIfixaties3(AOIs,ppx,ppy,scrRes,stimRect,imgScale)
+function [fixAOI] = detAOIfix(AOIs,ppx,ppy,scrRes,stimRect,imgScale)
 %
-% Loopt voor elke fixatie door alle AOIs heen en kijkt of de fixatie in die
-% AOI valt.
+% For each fixation (ppx, ppy), check if it is in any of the AOIs.
+% Fixations can be in multiple AOIs if AOIs overlap
 %
 % output (cell array):
 %   kolom 1: fixation ID, sequential by input
@@ -16,13 +16,13 @@ fixAOI(:,1) = num2cell(1:length(ppx));
 % filter out fixations not on the screen
 ppx         = ppx(:);
 ppy         = ppy(:);
-qOnscreen   = ppx>=1 & ppy>=1 & ppx<=scrRes(1) & ppy<=scrRes(2);    % deze boolean houden we bij voor fixaties die niet in een AOI vallen
+qOnscreen   = ppx>=1 & ppy>=1 & ppx<=scrRes(1) & ppy<=scrRes(2);
 
 fixAOI(~qOnscreen,2) = {-2};
 fixAOI(~qOnscreen,3) = {'off screen'};
 
 % filter fixations not on stimulus
-qOnstim     = ppx>=stimRect(1)+1/imgScale & ppy>=stimRect(2)+1/imgScale & ppx<=stimRect(3) & ppy<=stimRect(4);      % deze boolean houden we bij voor fixaties die niet in een AOI vallen
+qOnstim     = ppx>=stimRect(1)+1/imgScale & ppy>=stimRect(2)+1/imgScale & ppx<=stimRect(3) & ppy<=stimRect(4);
 osI         = find(qOnstim);
 
 fixAOI(qOnscreen&~qOnstim,2) = {-1};
@@ -47,37 +47,37 @@ if isempty(AOIs)
     return;
 end
 
-% maak 3D AOI boolean, alle AOIs voor deze stimulus achter elkaar in derde dimensie
-% collect AOI bools
+% make 3D AOI boolean, concat all AOIs for the stimulus along third
+% dimension
 sz          = size(AOIs(1).bool);
 bigAOIbool  = cat(3,AOIs.bool);
 AOInames    = {AOIs.name};
 AOInumbers  = 1:length(AOIs);
 
-%%% check welke fixaties in welke AOI vallen
-% reken fixaties om naar lineare indices in bigAOIbool
-indmat3D    = repmat(prod(sz)*[0:size(bigAOIbool,3)-1],length(wx),1);  % offset voor linear indices in derde dimensie
+%%% check which fixations are in which AOIs
+% turn fixation positions into linear indices into bigAOIbool
+indmat3D    = repmat(prod(sz)*[0:size(bigAOIbool,3)-1],length(wx),1);  %#ok<NBRAK1> - offset for linear indices in the third dimension
 inds        = repmat((wx-1)*sz(1)+wy,1,size(bigAOIbool,3));
 inds        = inds+indmat3D;
 
-% trek aan de hand van de indices per fixatie uit bigAOIbool of ze in AOI vallen of niet
-qAOI        = bigAOIbool(inds);                 % elke kolom is een AOI, elke rij is een fixatie
-qInAOI      = sum(qAOI,2)>0;                    % fixatie is in minstens een AOI gevallen
+% use linear indices to see which AOIs each fixation is in
+qAOI        = bigAOIbool(inds);                 % every column is an AOI, every row a fixation
+qInAOI      = sum(qAOI,2)>0;                    % fixation was in at least one AOI
 
-% nu resultaten in output
-% in geen enkele AOI
+%%% now add to output
+% deal with case of fixation that wasn't in any AOI
 fixAOI(osI(~qInAOI),2) = {0};
 fixAOI(osI(~qInAOI),3) = {'other'};
-% in AOIs gevallen
+% deal with fixations that fell in an AOI
 fixAOI(osI(qInAOI),:) = []; % remove and add per fixation, as some fixations are in multiple AOIs
 rows = find(qInAOI);
 for p=1:length(rows)
     r = rows(p);
     nAOI = sum(qAOI(r,:));
     temp = [num2cell([repmat(osI(r),1,nAOI); AOInumbers(qAOI(r,:))]).', AOInames(qAOI(r,:)).'];
-    fixAOI(end+[1:nAOI],:) = temp;
+    fixAOI(end+[1:nAOI],:) = temp; %#ok<NBRAK1> 
 end
 
 % sort by fix ID to keep original order
-[~,i] = sort([fixAOI{:,1}]);
+[~,i]  = sort([fixAOI{:,1}]);
 fixAOI = fixAOI(i,:);
