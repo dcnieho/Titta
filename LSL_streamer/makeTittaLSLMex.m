113981f8ab92820d3ae8f64a76193b25e6c6313e
Diederick Niehorster <dcnieho@gmail.com>
2025-08-12 14:04:24 +0200

    add Octave support to makeTittaLSLMex.m

--- LSL_streamer/makeTittaLSLMex.m
+++ LSL_streamer/makeTittaLSLMex.m
@@ -31,7 +31,81 @@ for SDK_version=1:2
 
 
     if isOctave
-        error("building on Octave is not supported");
+        if isWin
+            linkTobiiResearchLib = sprintf('-ltobii_research_v%d',SDK_version);
+        elseif isLinux
+            linkTobiiResearchLib = sprintf('-l:libtobii_research.so.%d',SDK_version);
+        else
+            linkTobiiResearchLib = sprintf('-ltobii_research.%d',SDK_version);
+        end
+        outFile = fullfile(outDir,sprintf('%s.%s',out_stem,mexext));
+        inpArgs = {'-v'
+            '-O3'
+            '-ffunction-sections'
+            '-fdata-sections'
+            '-flto'
+            '--output'
+            outFile
+            sprintf('-DTOBII_SDK_MAJOR_VERSION=%d',SDK_version)
+            '-DBUILD_FROM_SCRIPT'
+            '-DIS_OCTAVE'
+            sprintf('-I"%s"',fullfile(myDir,'deps','include'))
+            sprintf('-I"%s"',myDir)
+            sprintf('-I"%s"',fullfile(myDir,'..','SDK_wrapper'))
+            sprintf('-I"%s"',fullfile(myDir,'..','SDK_wrapper','deps','include'))
+            sprintf('-I"%s"',fullfile(myDir,'..','SDK_wrapper','deps','include',sprintf('SDKv%d',SDK_version)))
+            fullfile(myDir,'TittaLSLMex','TittaLSLMex.cpp')
+            fullfile(myDir,'src','TittaLSL.cpp')
+            fullfile(myDir,'..','SDK_wrapper','src','Titta.cpp')
+            fullfile(myDir,'..','SDK_wrapper','src','types.cpp')
+            fullfile(myDir,'..','SDK_wrapper','src','utils.cpp')
+            linkTobiiResearchLib
+            '-llsl'}.';
+
+        if isLinux
+            inpArgs = [inpArgs {
+                sprintf('-L%s',outDir)
+                '-lc'
+                '-lrt'
+                '-ldl'}.'];
+        elseif isOSX
+            inpArgs = [inpArgs {
+                sprintf('-L%s',outDir)
+                '-mmacosx-version-min=''11'''}.'];
+        elseif isWin
+            inpArgs = [inpArgs {
+                sprintf('-L"%s"',fullfile(myDir,'deps','lib'))
+                sprintf('-L"%s"',fullfile(myDir,'..','SDK_wrapper','deps','lib'))}.'];
+
+            % i need to switch path to bindir or mex/mkoctfile fails because
+            % gcc not found. Find proper solution for that later.
+            tdir=eval('__octave_config_info__("bindir")');  % eval because invalid syntax for matlab, would cause whole file not to run
+            cd(tdir);
+        end
+
+        % get cppflags, add to it what we need
+        flags = regexprep(mkoctfile('-p','CXXFLAGS'),'\r|\n','');   % strip newlines
+        if isempty(strfind(flags,'-std=c++2a')) %#ok<STREMP>
+            setenv('CXXFLAGS',[flags ' -std=c++2a']);
+        end
+        % get linker flags, add to it what we need
+        flags = regexprep(mkoctfile('-p','LDFLAGS'),'\r|\n','');   % strip newlines
+        flags = [flags ' -flto'];
+        if isLinux
+            flags = [flags ' -Wl,-rpath,''$ORIGIN'' -Wl,--gc-sections'];
+        elseif isOSX
+            flags = [flags ' -Wl,-rpath,''@loader_path'' -dead_strip -mmacosx-version-min=''11'''];
+        elseif isWin
+            flags = [flags ' -Wl,--gc-sections'];
+        end
+        setenv('LDFLAGS',flags);
+
+        mex(inpArgs{:});
+        cd(myDir);
+        if isLinux
+            % PTB does it, so we use their code to do this too
+            striplibsfrommexfile(outFile);
+        end
     else
         % prep input file
         cpp_file = fullfile(myDir,'TittaLSLMex',sprintf('%s.cpp',out_stem));
